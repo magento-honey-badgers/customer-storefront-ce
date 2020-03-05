dbff0403874b85f0b044037e8b56b2822faa4421
Cristian Partica <cpartica@magento.com>
2020-03-05 12:00:44 -0600

    MC-31550: ensure parity with schema and gql behavior
    - moving more logic in the resolver

--- app/code/Magento/CustomerStorefrontGraphQl/Model/Resolver/Customer.php
+++ app/code/Magento/CustomerStorefrontGraphQl/Model/Resolver/Customer.php
@@ -9,11 +9,15 @@ namespace Magento\CustomerStorefrontGraphQl\Model\Resolver;
 
 use Magento\CustomerStorefrontServiceApi\Api\CustomerRepositoryInterface;
 use Magento\CustomerStorefrontServiceApi\Api\Data\CustomerInterface;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\GraphQl\Model\Query\ContextInterface;
 
 /**
  * Customers field resolver
@@ -43,15 +47,23 @@ class Customer implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
+        /** @var \Magento\Framework\GraphQl\Query\Resolver\ContextExtensionInterface $extensionAttributes */
+        $extensionAttributes =  $context->getExtensionAttributes();
+        $currentUserId = $context->getUserId();
+        /** @var ContextInterface $context */
+        if (false === $extensionAttributes->getIsCustomer() || !$currentUserId) {
+            throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+        }
         try {
-            $currentUserId = $context->getUserId();
-            if ($currentUserId) {
-                $customer = $this->customerRepository->getById($currentUserId);
-            } else {
-                throw new GraphQlNoSuchEntityException(__('Customer not authenticated'));
-            }
+            $customer = $this->customerRepository->getById($currentUserId);
         } catch (NoSuchEntityException $e) {
-            throw new GraphQlNoSuchEntityException(__($e->getMessage()));
+            throw new GraphQlNoSuchEntityException(
+                __('Customer with id "%customer_id" does not exist.', ['customer_id' => $currentUserId]),
+                $e
+            );
+        } catch (LocalizedException $e) {
+            throw new GraphQlInputException(__($e->getMessage()));
+            // @codeCoverageIgnoreEnd
         }
         return $this->formatOutput($customer);
     }
@@ -70,6 +82,7 @@ class Customer implements ResolverInterface
         $customerArray = $customer->__toArray();
         $customerArray['customer_id'] = $customer->getId();
         $customerArray['date_of_birth'] = $customer->getDateOfBirth();
+        $customerArray['id'] = null;
         return $customerArray;
     }
 }
